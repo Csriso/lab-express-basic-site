a9841cacc21f9004bf6675ab3049d8376f014e9a
unknown <cesyrub@gmail.com>
2022-05-03 16:35:39 +0200

    routes

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ app.use(express.static("public")); // esto indica a express donde estan ubicados
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
